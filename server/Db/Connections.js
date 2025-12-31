91c6e8d62c62075e1cf4f2de88f97f6abcfb35c8
Mohammad Suhail Shaik <suhailshaikmohammad129@gmail.com>
2026-01-01 03:14:48 +0530

    db fixed

--- server/Db/Connections.js
+++ server/Db/Connections.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI) 
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
